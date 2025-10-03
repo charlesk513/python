d07777796f111c941786d913e745f8c7ca8ac3a0
Kabunga Charles <kcjesuschrist513@gmail.com>
2025-10-03 15:02:28 +0300

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
-
 x = []
 y = []
+# these are the prompts for titles
 title_given = input("\nEnter the title you want for the graph: ")
 x_axis = input("Enter the label you want for x axis: ")
 y_axis = input("Enter the label you want for y axis: ")
+
+#the user enters the number of points to plot and the value entered determines the number of iterations for x and y values
 number_of_points = int(input("\nEnter the number of points you want to plot: "))
 for n in range(number_of_points):
     x_values = int(input("\nEnter the number of x values you want to plot: "))
